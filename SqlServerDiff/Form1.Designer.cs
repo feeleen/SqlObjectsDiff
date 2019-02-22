c73582807788458367190a05fa174f345e573e0b
me <Skalkin.Vladimir@vitta.ru>
2019-02-22 16:06:23 +0300

    Modified   SqlServerDiff/Form1.Designer.cs Modified   SqlServerDiff/Form1.cs

--- SqlServerDiff/Form1.Designer.cs
+++ SqlServerDiff/Form1.Designer.cs
@@ -30,18 +30,31 @@
 		{
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Tables");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Triggers");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Stored Procedures");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Functions");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("User Types");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Views");
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.fctb = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.AnalyzeDiffBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.fctb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(696, 40);
+            this.button1.Location = new System.Drawing.Point(698, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(99, 23);
             this.button1.TabIndex = 0;
@@ -77,9 +90,6 @@
             // 
             // fctb
             // 
-            this.fctb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.fctb.AutoCompleteBracketsList = new char[] {
         '(',
         ')',
@@ -103,12 +113,13 @@
             this.fctb.DelayedEventsInterval = 200;
             this.fctb.DelayedTextChangedInterval = 500;
             this.fctb.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fctb.Dock = System.Windows.Forms.DockStyle.Fill;
             this.fctb.Font = new System.Drawing.Font("Consolas", 9.75F);
             this.fctb.ImeMode = System.Windows.Forms.ImeMode.Off;
             this.fctb.IsReplaceMode = false;
             this.fctb.Language = FastColoredTextBoxNS.Language.SQL;
             this.fctb.LeftBracket = '(';
-            this.fctb.Location = new System.Drawing.Point(3, 40);
+            this.fctb.Location = new System.Drawing.Point(0, 0);
             this.fctb.Name = "fctb";
             this.fctb.Paddings = new System.Windows.Forms.Padding(0);
             this.fctb.PreferredLineWidth = 80;
@@ -116,16 +127,74 @@
             this.fctb.RightBracket = ')';
             this.fctb.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
             this.fctb.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fctb.ServiceColors")));
-            this.fctb.Size = new System.Drawing.Size(687, 406);
+            this.fctb.Size = new System.Drawing.Size(525, 406);
             this.fctb.TabIndex = 5;
             this.fctb.Zoom = 100;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "NodeTables";
+            treeNode1.Text = "Tables";
+            treeNode2.Name = "NodeTriggers";
+            treeNode2.Text = "Triggers";
+            treeNode3.Name = "NodeSP";
+            treeNode3.Text = "Stored Procedures";
+            treeNode4.Name = "NodeFn";
+            treeNode4.Text = "Functions";
+            treeNode5.Name = "NodeUserTypes";
+            treeNode5.Text = "User Types";
+            treeNode6.Name = "NodeViews";
+            treeNode6.Text = "Views";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6});
+            this.treeView1.Size = new System.Drawing.Size(264, 406);
+            this.treeView1.TabIndex = 6;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(4, 41);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.fctb);
+            this.splitContainer1.Size = new System.Drawing.Size(793, 406);
+            this.splitContainer1.SplitterDistance = 264;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // AnalyzeDiffBtn
+            // 
+            this.AnalyzeDiffBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.AnalyzeDiffBtn.Location = new System.Drawing.Point(617, 12);
+            this.AnalyzeDiffBtn.Name = "AnalyzeDiffBtn";
+            this.AnalyzeDiffBtn.Size = new System.Drawing.Size(75, 23);
+            this.AnalyzeDiffBtn.TabIndex = 8;
+            this.AnalyzeDiffBtn.Text = "Analyze Diff";
+            this.AnalyzeDiffBtn.UseVisualStyleBackColor = true;
+            this.AnalyzeDiffBtn.Click += new System.EventHandler(this.AnalyzeDiffBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.fctb);
+            this.Controls.Add(this.AnalyzeDiffBtn);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -133,6 +202,10 @@
             this.Name = "Form1";
             this.Text = "Сверка изменений с тестовой базой";
             ((System.ComponentModel.ISupportInitialize)(this.fctb)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,6 +218,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private FastColoredTextBoxNS.FastColoredTextBox fctb;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button AnalyzeDiffBtn;
     }
 }
 
--- SqlServerDiff/Form1.cs
+++ SqlServerDiff/Form1.cs
@@ -13,16 +13,39 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Feeleen.Diff;
+using System.Data.SqlClient;
 
 namespace SqlServerDiff
 {
     public partial class Form1 : Form
     {
 
-        ServerConnection mainConn = new ServerConnection("MAINSERV");
-        ServerConnection testConn = new ServerConnection("TEMPSERV");
+        ServerConnection mainConn = new ServerConnection("ds");
+        ServerConnection testConn = new ServerConnection("TEMP-sql1");
 
-        Dictionary<string, string> triggersToTables = new Dictionary<string, string>();
+        Dictionary<string, string> triggersToTablesMain = new Dictionary<string, string>();
+        Dictionary<string, string> triggersToTablesTest = new Dictionary<string, string>();
+
+        Dictionary<string, string> SPTextMain = new Dictionary<string, string>();
+        Dictionary<string, string> SPTextTest = new Dictionary<string, string>();
+
+        Dictionary<string, string> TriggerTextMain = new Dictionary<string, string>();
+        Dictionary<string, string> TriggerTextTest = new Dictionary<string, string>();
+
+        Dictionary<string, string> ViewTextMain = new Dictionary<string, string>();
+        Dictionary<string, string> ViewTextTest = new Dictionary<string, string>();
+
+        Dictionary<string, string> UFTextMain = new Dictionary<string, string>();
+        Dictionary<string, string> UFTextTest = new Dictionary<string, string>();
+
+        Dictionary<string, string> UTTextMain = new Dictionary<string, string>();
+        Dictionary<string, string> UTTextTest = new Dictionary<string, string>();
+
+        Dictionary<string, string> TableTextMain = new Dictionary<string, string>();
+        Dictionary<string, string> TableTextTest = new Dictionary<string, string>();
+
+        Dictionary<string, string> ChangedObjectsMain = new Dictionary<string, string>();
+        Dictionary<string, string> ChangedObjectsTest = new Dictionary<string, string>();
 
         public Server MainServer
         {
@@ -36,12 +59,12 @@ namespace SqlServerDiff
 
         public Database MainDB
         {
-            get { return MainServer.Databases["mydb"]; }
+            get { return MainServer.Databases["drugman"]; }
         }
 
         public Database TestDB
         {
-            get { return TestServer.Databases["mydb"]; }
+            get { return TestServer.Databases["drugman1"]; }
         }
 
         public Server TestServer
@@ -53,9 +76,24 @@ namespace SqlServerDiff
             }
         }
 
-		public Form1()
+        TreeNode NodeTables = new TreeNode("Tables");
+        TreeNode NodeViews = new TreeNode("Views");
+        TreeNode NodeSP = new TreeNode("Stored Procedures");
+        TreeNode NodeUT = new TreeNode("User Types");
+        TreeNode NodeUF = new TreeNode("User Functions");
+        TreeNode NodeTriggers = new TreeNode("Triggers");
+
+        public Form1()
 		{
 			InitializeComponent();
+
+            treeView1.Nodes.Clear();
+            treeView1.Nodes.Add(NodeTables);
+            treeView1.Nodes.Add(NodeViews);
+            treeView1.Nodes.Add(NodeSP);
+            treeView1.Nodes.Add(NodeUT);
+            treeView1.Nodes.Add(NodeUF);
+            treeView1.Nodes.Add(NodeTriggers);
 
             mainConn.LoginSecure = false;
             testConn.LoginSecure = false;
@@ -74,41 +112,44 @@ namespace SqlServerDiff
             mainConn.Connect();
             testConn.Connect();
 
-            foreach (Database myDatabase in MainServer.Databases)
-			{
-				Console.WriteLine(myDatabase.Name);
-			}
-			
-			foreach (Table myTable in MainDB.Tables)
-			{
-				Console.WriteLine(myTable.Name);
-			}
-
-			foreach (StoredProcedure myStoredProcedure in MainDB.StoredProcedures)
-			{
-				Console.WriteLine(myStoredProcedure.Name);
-			}
+            ChangedObjectsTest = GetChangedObjects(TestDB);
 
 
             foreach (Table tbl in MainDB.Tables)
             {
                 foreach (Trigger tr in tbl.Triggers)
                 {
-                    triggersToTables[tr.Name] = tbl.Name;
+                    triggersToTablesMain[tr.Name] = tbl.Name;
                 }
             }
 
-            //List down all the user-defined function of AdventureWorks
-            foreach (UserDefinedFunction myUserDefinedFunction in MainDB.UserDefinedFunctions)
-			{
-				Console.WriteLine(myUserDefinedFunction.Name);
-			}
-			//List down all the properties and its values of [HumanResources].[Employee] table
-			//foreach (Property myTableProperty in MainDB.Tables["tblInvoices", "dbo"].Properties)
-			//{
-			//	Console.WriteLine(myTableProperty.Name + " : " + myTableProperty.Value);
-			//}
 		}
+
+        private Dictionary<string, string> GetChangedObjects(Database db)
+        {
+            string sql = $@"use {db.Name};
+                select
+                    name,
+                    type,
+                    type_desc
+                from sys.all_objects
+                where modify_date > dateadd(m, -3, getdate())
+                order by modify_date desc";
+
+            DataSet set = db.ExecuteWithResults(sql);
+
+            Dictionary<string, string> objects = new Dictionary<string, string>();
+
+            foreach (DataTable t in set.Tables)
+            {
+                foreach (DataRow row in t.Rows)
+                    objects[row["name"].ToString()] = row["type"].ToString();
+                break;
+            }
+
+            return objects;
+        }
+
 
         private void TextArea_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -147,13 +188,52 @@ namespace SqlServerDiff
             return db.StoredProcedures[name].TextHeader + Environment.NewLine + db.StoredProcedures[name].TextBody;
         }
 
+        public string GetUTText(Database db, string name)
+        {
+            if (db.UserDefinedTypes[name] == null)
+                return null;
+
+            StringBuilder builder = new StringBuilder();
+            foreach (string s in db.UserDefinedTypes[name].Script())
+            {
+                builder.AppendLine(s);
+            }
+            string result = builder.ToString();
+
+            return result;
+        }
+
+        public string GetUFText(Database db, string name)
+        {
+            if (db.UserDefinedFunctions[name] == null)
+                return null;
+
+            return db.UserDefinedFunctions[name].TextHeader + Environment.NewLine + db.UserDefinedFunctions[name].TextBody;
+        }
+
+        public string GetTableText(Database db, string name)
+        {
+            if (db.Tables[name] == null)
+                return null;
+
+            StringBuilder builder = new StringBuilder();
+            foreach (string s in db.Tables[name].Script())
+            {
+                builder.AppendLine(s);
+            }
+            string result = builder.ToString();
+
+            return result;
+        }
+
+
         public string GetTriggerText(Database db, string name)
         {
             string tableName = "";
 
-            if (triggersToTables.ContainsKey(name) && triggersToTables[name] != null)
+            if (triggersToTablesMain.ContainsKey(name) && triggersToTablesMain[name] != null)
             {
-                tableName = triggersToTables[name];
+                tableName = triggersToTablesMain[name];
             }
             else
                 return null;
@@ -175,7 +255,7 @@ namespace SqlServerDiff
             {
                 ShowDifferences(GetStoredProcedureText(MainDB, textBox1.Text.Trim()), GetStoredProcedureText(TestDB, textBox1.Text.Trim()), DiffType.Text);
             }
-            else if (triggersToTables.ContainsKey(textBox1.Text.Trim()) && triggersToTables[textBox1.Text.Trim()] != null)
+            else if (triggersToTablesMain.ContainsKey(textBox1.Text.Trim()) && triggersToTablesMain[textBox1.Text.Trim()] != null)
             {
                 ShowDifferences(GetTriggerText(MainDB, textBox1.Text.Trim()), GetTriggerText(TestDB, textBox1.Text.Trim()), DiffType.Text);
             }
@@ -205,6 +285,115 @@ namespace SqlServerDiff
             //{
              //   Console.WriteLine(myUserDefinedFunction.Name);
             //}
+        }
+
+        private void AnalyzeDiffBtn_Click(object sender, EventArgs e)
+        {
+            GetObjects(MainDB);
+            GetObjects(TestDB);
+        }
+
+        private void GetObjects(Database db)
+        {
+            foreach (Table tbl in db.Tables)
+            {
+                if (!ChangedObjectsTest.ContainsKey(tbl.Name))
+                    continue;
+
+                if (db == MainDB)
+                {
+                    TableTextMain[tbl.Name] = GetTableText(db, tbl.Name);
+                }
+                else
+                {
+                    TableTextTest[tbl.Name] = GetTableText(db, tbl.Name);
+                }
+
+                if (!NodeTables.Nodes.ContainsKey(tbl.Name))
+                {
+                    TreeNode nt = new TreeNode(tbl.Name);
+                    nt.Name = tbl.Name;
+                    NodeTables.Nodes.Add(nt);
+                }
+
+                foreach (Trigger tr in tbl.Triggers)
+                {
+                    if (!ChangedObjectsTest.ContainsKey(tr.Name) || ChangedObjectsTest[tr.Name] != "TR")
+                        continue;
+
+                    if (db == MainDB)
+                    {
+                        triggersToTablesMain[tr.Name] = tbl.Name;
+                    }
+                    else
+                    {
+                        triggersToTablesTest[tr.Name] = tbl.Name;
+                    }
+                }
+            }
+
+
+            foreach (Microsoft.SqlServer.Management.Smo.View v in db.Views)
+            {
+                if (!ChangedObjectsTest.ContainsKey(v.Name) || ChangedObjectsTest[v.Name] != "V")
+                    continue;
+
+                if (db == MainDB)
+                {
+                    ViewTextMain[v.Name] = GetViewText(db, v.Name);
+                }
+                else
+                {
+                    ViewTextTest[v.Name] = GetViewText(db, v.Name);
+                }
+            }
+
+
+            foreach (StoredProcedure sp in db.StoredProcedures)
+            {
+                if (!ChangedObjectsTest.ContainsKey(sp.Name) || ChangedObjectsTest[sp.Name] != "P")
+                    continue;
+
+                if (db == MainDB)
+                {
+                    SPTextMain[sp.Name] = GetStoredProcedureText(db, sp.Name);
+                }
+                else
+                {
+                    SPTextTest[sp.Name] = GetStoredProcedureText(db, sp.Name);
+                }
+            }
+
+            foreach (UserDefinedFunction uf in db.UserDefinedFunctions)
+            {
+                if (!ChangedObjectsTest.ContainsKey(uf.Name))// || ChangedObjectsTest[uf.Name] != )
+                    continue;
+
+                if (db == MainDB)
+                {
+                    UFTextMain[uf.Name] = GetUFText(db, uf.Name);
+                }
+                else
+                {
+                    UFTextTest[uf.Name] = GetUFText(db, uf.Name);
+                }
+            }
+
+            foreach (UserDefinedType ut in db.UserDefinedTypes)
+            {
+                if (!ChangedObjectsTest.ContainsKey(ut.Name) || ChangedObjectsTest[ut.Name] != "TT")
+                    continue;
+
+
+                if (db == MainDB)
+                {
+                    UFTextMain[ut.Name] = GetUFText(db, ut.Name);
+                }
+                else
+                {
+                    UFTextTest[ut.Name] = GetUFText(db, ut.Name);
+                }
+            }
         }
     }
 }
