6bedd0db196ba599c249ca38a1807fe8b972c30d
me <Skalkin.Vladimir@vitta.ru>
2019-02-25 09:19:57 +0300

    Modified   SqlServerDiff/Form1.Designer.cs

--- SqlServerDiff/Form1.Designer.cs
+++ SqlServerDiff/Form1.Designer.cs
@@ -77,17 +77,17 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(1, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.Size = new System.Drawing.Size(76, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Поиск объекта:";
+            this.label1.Text = "Object search:";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(461, 12);
+            this.button2.Location = new System.Drawing.Point(457, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "Искать";
+            this.button2.Text = "Search";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -229,7 +229,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Сверка изменений с тестовой базой";
+            this.Text = "SqlServerDiff - Compare objects between Main and Test";
             ((System.ComponentModel.ISupportInitialize)(this.fctb)).EndInit();
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
